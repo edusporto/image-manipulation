502f7f39009da8b18683ef6fee7290d4dd03928b
Eduardo Sandalo Porto <eduardosandaloporto@gmail.com>
2019-08-07 12:11:00 -0300

    Simple image editing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+extern crate image;
+extern crate rayon;
+
+use rayon::prelude::*;
+
+use image::{Pixel};
+
 fn main() {
+    let mut img = image::open("image.png").unwrap().to_rgb();
     
+    let gray = image::Rgb([128_u8, 128, 128]);
+    
+    for pixel in img.pixels_mut() {
+        let rgb = pixel.to_rgb();
+    
+        if rgb[0] > 240 {
+            if rgb[1] > 240 {
+                if rgb[2] > 240 {
+                    *pixel = gray;
+                }
+            }
+        }
+
+        /*if pixel.to_rgb() == white {
+            *pixel = gray;
+        }*/
+    }
+
+    img.save("output.png");
 }
