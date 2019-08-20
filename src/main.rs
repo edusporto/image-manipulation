8678363ec63aecdee7028f62cf1771d74c6e4c4b
Eduardo Sandalo Porto <eduardosandaloporto@gmail.com>
2019-08-20 13:38:53 -0300

    Testing

--- src/main.rs
+++ src/main.rs
@@ -2,63 +2,41 @@ extern crate image;
 
 use std::error::Error;
 
-fn calc_location(
-    size2: (u32, u32),
-    location: (u32, u32),
-) -> (u32, u32) {
-    
-    if location.0 < size2.0 && location.1 < size2.1 {
+fn calc_location(size_pattern: (u32, u32), location: (u32, u32)) -> (u32, u32) {
+    if location.0 < size_pattern.0 && location.1 < size_pattern.1 {
         return location;
     }
 
-    let mut ret = (0, 0);
-
-    let a = location.0 / size2.0;
-    let b = location.1 / size2.1;
-
-    ret.0 = location.0 - size2.0 * a;
-    ret.1 = location.1 - size2.1 * b;
-
-    ret
+    (
+        location.0 - size_pattern.0 * (location.0 / size_pattern.0),
+        location.1 - size_pattern.1 * (location.1 / size_pattern.1),
+    )
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let mut img = image::open("image.png").unwrap().to_rgb();
-<<<<<<< HEAD
-    let pattern = image::open("pattern.jpg").unwrap().to_rgb();
+    let mut img = image::open("image.png")?.to_rgb();
+    let pattern1 = image::open("pattern1.png")?.to_rgb();
+    let pattern2 = image::open("pattern2.png")?.to_rgb();
 
-    let old_color = image::Rgb([209, 255, 125]);
-    // let new_color = image::Rgb([20, 20, 240]);
-    //let mut pixels_to_change = Vec::<image::Rgb<u8>>::new();
-=======
-    
-    let gray = image::Rgb([128_u8, 128, 128]);
-    
-    // TODO: use rayon
-    // https://rust-lang-nursery.github.io/rust-cookbook/concurrency/parallel.html
-    // https://stackoverflow.com/questions/48922420/how-do-i-use-rayon-with-an-existing-iterator/54201274#54201274
+    let color1 = image::Rgb([209, 255, 125]);
+    let color2 = image::Rgb([149, 190, 0]);
 
-    for pixel in img.pixels_mut() {
-        let rgb = pixel.to_rgb();
-    
-        if rgb[0] > 240 {
-            if rgb[1] > 240 {
-                if rgb[2] > 240 {
-                    *pixel = gray;
-                }
-            }
-        }
->>>>>>> a56a0e3a9236d91d76c902b2ebd1f6bd51628b83
-
-    let size1 = (img.width(), img.height());
-    let size2 = (pattern.width(), pattern.height());
+    let size1 = (pattern1.width(), pattern1.height());
+    let size2 = (pattern2.width(), pattern2.height());
 
     for p in img.enumerate_pixels_mut() {
-        if *p.2 == old_color {
+        if *p.2 == color1 {
+            //pixels_to_change.push(*p);
+
+            let loc = calc_location(size1, (p.0, p.1));
+            *p.2 = *pattern1.get_pixel(loc.0, loc.1);
+        }
+
+        if *p.2 == color2 {
             //pixels_to_change.push(*p);
 
             let loc = calc_location(size2, (p.0, p.1));
-            *p.2 = *pattern.get_pixel(loc.0, loc.1);
+            *p.2 = *pattern2.get_pixel(loc.0, loc.1);
         }
     }
 
