9f894b44a045526e030acc92cd5229970b9f45d8
Eduardo Sandalo Porto <eduardosandaloporto@gmail.com>
2019-08-20 12:48:45 -0300

    Substituting colors for images

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,48 @@
 extern crate image;
-extern crate rayon;
 
-use rayon::prelude::*;
+use std::error::Error;
 
-use image::{Pixel};
-
-fn main() {
-    let mut img = image::open("image.png").unwrap().to_rgb();
+fn calc_location(
+    size2: (u32, u32),
+    location: (u32, u32),
+) -> (u32, u32) {
     
-    let gray = image::Rgb([128_u8, 128, 128]);
-    
-    for pixel in img.pixels_mut() {
-        let rgb = pixel.to_rgb();
-    
-        if rgb[0] > 240 {
-            if rgb[1] > 240 {
-                if rgb[2] > 240 {
-                    *pixel = gray;
-                }
-            }
-        }
-
-        /*if pixel.to_rgb() == white {
-            *pixel = gray;
-        }*/
+    if location.0 < size2.0 && location.1 < size2.1 {
+        return location;
     }
 
-    img.save("output.png");
+    let mut ret = (0, 0);
+
+    let a = location.0 / size2.0;
+    let b = location.1 / size2.1;
+
+    ret.0 = location.0 - size2.0 * a;
+    ret.1 = location.1 - size2.1 * b;
+
+    ret
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut img = image::open("image.png").unwrap().to_rgb();
+    let pattern = image::open("pattern.jpg").unwrap().to_rgb();
+
+    let old_color = image::Rgb([209, 255, 125]);
+    // let new_color = image::Rgb([20, 20, 240]);
+    //let mut pixels_to_change = Vec::<image::Rgb<u8>>::new();
+
+    let size1 = (img.width(), img.height());
+    let size2 = (pattern.width(), pattern.height());
+
+    for p in img.enumerate_pixels_mut() {
+        if *p.2 == old_color {
+            //pixels_to_change.push(*p);
+
+            let loc = calc_location(size2, (p.0, p.1));
+            *p.2 = *pattern.get_pixel(loc.0, loc.1);
+        }
+    }
+
+    img.save("output.png")?;
+
+    Ok(())
 }
