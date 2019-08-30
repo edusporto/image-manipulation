dd70f3f01a37bd2d3ec98637ef81d7d10fbb9f02
Eduardo Sandalo Porto <eduardosandaloporto@gmail.com>
2019-08-30 16:17:55 -0300

    A few changes

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,9 @@ extern crate image;
 
 use std::error::Error;
 
+// TODO: check polyfloyd/edge-detection-rs repository on GitHub
+// TODO: test on Android
+
 fn calc_location(size_pattern: (u32, u32), location: (u32, u32)) -> (u32, u32) {
     if location.0 < size_pattern.0 && location.1 < size_pattern.1 {
         return location;
@@ -25,12 +28,17 @@ fn main() -> Result<(), Box<dyn Error>> {
     let color1 = image::Rgb([209, 255, 125]);
     let color2 = image::Rgb([149, 190, 0]);
     let color3 = image::Rgb([158, 20, 43]);
-    //let white = image::Rgb([255, 255, 255]);
-    //let black = image::Rgb([0, 0, 0]);
 
     let size1 = (pattern1.width(), pattern1.height());
     let size2 = (pattern2.width(), pattern2.height());
     let size3 = (pattern3.width(), pattern3.height());
+
+    // Processing
+
+    let edges = imageproc::edges::canny(&img_luma, 10.0, 50.0);
+    //let color_edges = imageproc::map::map_colors(&edges, |p| if p[0] > 0 { white } else { black });
+
+    edges.save("edges.png")?;
 
     for pixel in img.enumerate_pixels_mut() {
         if *pixel.2 == color1 {
@@ -48,11 +56,6 @@ fn main() -> Result<(), Box<dyn Error>> {
             *pixel.2 = *pattern3.get_pixel(loc.0, loc.1);
         }
     }
-
-    let edges = imageproc::edges::canny(&img_luma, 50.0, 100.0);
-    //let color_edges = imageproc::map::map_colors(&edges, |p| if p[0] > 0 { white } else { black });
-
-    edges.save("lines.png")?;
 
     img.save("output.png")?;
 
