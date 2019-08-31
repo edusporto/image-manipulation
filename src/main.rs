f86f9e116a325993519e37e10a8a0056110d21e4
Eduardo Sandalo Porto <30930225+edusporto@users.noreply.github.com>
2019-08-31 14:59:03 -0300

    Improved code

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 extern crate image;
+use image::{ImageBuffer, Rgb};
 
 use std::error::Error;
 
@@ -16,22 +17,30 @@ fn calc_location(size_pattern: (u32, u32), location: (u32, u32)) -> (u32, u32) {
     )
 }
 
+fn draw_pattern(
+    pixel: &mut (u32, u32, &mut image::Rgb<u8>),
+    pattern: &ImageBuffer<Rgb<u8>, Vec<u8>>,
+) {
+    let loc = calc_location((pattern.width(), pattern.height()), (pixel.0, pixel.1));
+    *pixel.2 = *pattern.get_pixel(loc.0, loc.1);
+}
+
 fn main() -> Result<(), Box<dyn Error>> {
     let dyn_img = image::open("image.png")?;
     let mut img = dyn_img.to_rgb();
     let img_luma = dyn_img.to_luma();
 
-    let pattern1 = image::open("patterns/0.png")?.to_rgb();
-    let pattern2 = image::open("patterns/1.png")?.to_rgb();
-    let pattern3 = image::open("patterns/2.png")?.to_rgb();
+    let patterns = vec![
+        image::open("patterns/0.png")?.to_rgb(),
+        image::open("patterns/1.png")?.to_rgb(),
+        image::open("patterns/2.png")?.to_rgb(),
+    ];
 
-    let color1 = image::Rgb([209, 255, 125]);
-    let color2 = image::Rgb([149, 190, 0]);
-    let color3 = image::Rgb([158, 20, 43]);
-
-    let size1 = (pattern1.width(), pattern1.height());
-    let size2 = (pattern2.width(), pattern2.height());
-    let size3 = (pattern3.width(), pattern3.height());
+    let colors = vec![
+        image::Rgb([209, 255, 125]),
+        image::Rgb([149, 190, 0]),
+        image::Rgb([158, 20, 43]),
+    ];
 
     // Processing
 
@@ -40,20 +49,11 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     edges.save("edges.png")?;
 
-    for pixel in img.enumerate_pixels_mut() {
-        if *pixel.2 == color1 {
-            let loc = calc_location(size1, (pixel.0, pixel.1));
-            *pixel.2 = *pattern1.get_pixel(loc.0, loc.1);
-        }
-
-        if *pixel.2 == color2 {
-            let loc = calc_location(size2, (pixel.0, pixel.1));
-            *pixel.2 = *pattern2.get_pixel(loc.0, loc.1);
-        }
-
-        if *pixel.2 == color3 {
-            let loc = calc_location(size3, (pixel.0, pixel.1));
-            *pixel.2 = *pattern3.get_pixel(loc.0, loc.1);
+    for mut pixel in img.enumerate_pixels_mut() {
+        for (i, color) in colors.iter().enumerate() {
+            if pixel.2 == color {
+                draw_pattern(&mut pixel, &patterns[i]);
+            }
         }
     }
 
