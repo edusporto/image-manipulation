84910db5b1b40db04754d8c32efcfd9135496667
Eduardo Sandalo Porto <30930225+edusporto@users.noreply.github.com>
2019-09-01 20:22:45 -0300

    Changes

--- README.md
+++ README.md
@@ -2,5 +2,6 @@
 Experimenting with image manipulation in Rust.
 
 Examples:
-```cargo run --release image.png "840 500 5" "400 400 5" "400 200 5"```
-```cargo run --release rome.jpg "170 240 50" "370 315 50" "450 350 20"```
+```cargo run --release image.png "840 500 5 5 5" "400 400 5 5 5" "400 200 5 5 5"```
+```cargo run --release rome.jpg "170 240 50 50 50" "370 315 50 50 50" "450 350 20 20 20"```
+```cargo run --release asia.jpg "690 240 20 20 20" "315 340 10 10 10" "200 525 10 10 10"```
--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,13 @@ const BLACK: Rgb<u8> = Rgb([0, 0, 0]);
 type Image = ImageBuffer<Rgb<u8>, Vec<u8>>;
 type Color = Rgb<u8>;
 
+#[derive(Debug, Copy, Clone)]
+pub struct ColorThreshold {
+    pub red: u8,
+    pub green: u8,
+    pub blue: u8,
+}
+
 pub fn calc_location(size_pattern: (u32, u32), location: (u32, u32)) -> (u32, u32) {
     if location.0 < size_pattern.0 && location.1 < size_pattern.1 {
         return location;
@@ -26,12 +33,24 @@ fn lighten(color: Color) -> Color {
     ])
 }
 
+fn darken(color: Color) -> Color {
+    Rgb([color[0] / 4, color[1] / 4, color[2] / 4])
+}
+
+fn is_light(color: Color) -> bool {
+    color[0] > 204 && color[1] > 204 && color[2] > 204
+}
+
 pub fn draw_pattern(pixel: &mut (u32, u32, &mut Color), pattern: &Image, color: Color) {
     let loc = calc_location((pattern.width(), pattern.height()), (pixel.0, pixel.1));
     *pixel.2 = if *pattern.get_pixel(loc.0, loc.1) == BLACK {
         color
     } else {
-        lighten(color)
+        if is_light(color) {
+            darken(color)
+        } else {
+            lighten(color)
+        }
     }
 }
 
@@ -47,10 +66,20 @@ fn between(smaller: u8, val: u8, bigger: u8) -> bool {
     smaller <= val && val <= bigger
 }
 
-pub fn color_in_range(query: Color, color: Color, threshold: u8) -> bool {
-    between(sub(query[0], threshold), color[0], add(query[0], threshold))
-        && between(sub(query[1], threshold), color[1], add(query[1], threshold))
-        && between(sub(query[2], threshold), color[2], add(query[2], threshold))
+pub fn color_in_range(query: Color, color: Color, threshold: ColorThreshold) -> bool {
+    between(
+        sub(query[0], threshold.red),
+        color[0],
+        add(query[0], threshold.red),
+    ) && between(
+        sub(query[1], threshold.green),
+        color[1],
+        add(query[1], threshold.green),
+    ) && between(
+        sub(query[2], threshold.blue),
+        color[2],
+        add(query[2], threshold.blue),
+    )
 }
 
 pub fn read_patterns(dir: &str) -> Result<Vec<Image>, Box<dyn Error>> {
@@ -70,28 +99,47 @@ pub fn read_patterns(dir: &str) -> Result<Vec<Image>, Box<dyn Error>> {
 
 use std::env::Args;
 
-pub fn read_colors(args: &mut Args, img: &Image) -> Result<Vec<(Color, u8)>, Box<dyn Error>> {
+#[derive(Debug)]
+struct ColorPickingError(&'static str);
+
+impl std::fmt::Display for ColorPickingError {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(
+            f,
+            "Error when choosing which colors to change to patterns: {}",
+            self.0
+        )
+    }
+}
+
+impl Error for ColorPickingError {}
+
+pub fn read_colors(
+    args: &mut Args,
+    img: &Image,
+) -> Result<Vec<(Color, ColorThreshold)>, Box<dyn Error>> {
     let mut colors = Vec::new();
 
-    for coords in args {
-        let mut coords = coords.split(' ');
+    for color_options in args {
+        let color_options: Vec<&str> = color_options.split(' ').collect();
 
-        let x: u32 = coords
-            .next()
-            .ok_or("Could not find x coordinate for a color")?
-            .parse()?;
+        if color_options.len() < 2 {
+            return Result::Err(Box::new(ColorPickingError("Missing color coordinates")));
+        }
 
-        let y: u32 = coords
-            .next()
-            .ok_or("Could not find y coordinate for a color")?
-            .parse()?;
+        if color_options.len() < 5 {
+            return Result::Err(Box::new(ColorPickingError(
+                "Missing color threshold values",
+            )));
+        }
 
-        let threshold: u8 = coords
-            .next()
-            .ok_or("Could not read threshold value for a color")?
-            .parse()?;
+        let x: u32 = color_options[0].parse()?;
+        let y: u32 = color_options[1].parse()?;
+        let red: u8 = color_options[2].parse()?;
+        let green: u8 = color_options[3].parse()?;
+        let blue: u8 = color_options[4].parse()?;
 
-        colors.push((*img.get_pixel(x, y), threshold));
+        colors.push((*img.get_pixel(x, y), ColorThreshold { red, green, blue }));
     }
 
     Ok(colors)
